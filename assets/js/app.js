2d5dbcd5712009f2aee0d7a43aded29e7caadb36
Raiden <syedzohaibak@gmail.com>
2018-03-17 23:36:13 +0530

    Items completed and Toggle functionality added

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,31 +3,37 @@ var mainApp  =  angular.module("mainApp" , []);
 mainApp.controller("todoController" , function($scope){
 
     $scope.todo = [];
+    $scope.completed = [];
     $scope.count = 0;
+    $scope.completedCount = 0;
     $scope.onSubmit = function(){
         $scope.todo.push({"name": $scope.todoName,"active":false});
         console.log($scope.todo);
         $scope.count++;
-    }
-    
-    // $scope.toggleActive = function(s){
-    //     s.active  = !s.active;
-    // }
-    $scope.sub = function(s){
-    for(var i=0;i< $scope.todo.length;i++)
-    {
-        // console.log($scope.todo[i].active)
-       
-        if($scope.todo[i].active == true)
-        {
-            $scope.count--;
-            
-            
+    };
+    $scope.deleteTodo = function($index){
+            $scope.todo.splice($index,1);
+            console.log($index);
+        };
+    $scope.deleteCompleted = function($index){
+            $scope.completed.splice($index,1);
+        };
+    $scope.itemCompleted = function($index){
+            if($scope.todo[$index].active == true){
+                // console.log($scope.todo[$index].name);
+                $scope.completed.push({"name":$scope.todo[$index].name, "active":true});
+                $scope.deleteTodo($index);
+                $scope.count--;
+                $scope.completedCount++;
+                // console.log($scope.completed);
         }
-        else if($scope.todo[i].active == false){
-            $scope.count++;
+    };
+    $scope.itemTodoAgain = function($index){
+            if($scope.completed[$index].active == false){
+                $scope.todo.push({"name":$scope.completed[$index].name, "active":false});
+                $scope.deleteCompleted($index);
+                $scope.completedCount--;
+                $scope.count++;  
         }
-       
-    }
-};
+    };
 })
